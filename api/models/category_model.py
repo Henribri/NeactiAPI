c69535ee8befe72555bcef8693a15b207da9fa63
Henribri <henri.briaux@viacesi.fr>
2020-05-21 22:48:30 +0200

    good default

--- api/models/category_model.py
+++ api/models/category_model.py
@@ -1,7 +1,7 @@
 from djongo import models
 class Category(models.Model):
-    name=models.CharField(default='Sport', max_length=100)
-    iconId=models.IntegerField(default=57392)
+    name=models.CharField(default='Jeux-video', max_length=100)
+    iconId=models.IntegerField(default=58168)
     fontFamily=models.CharField(default='MaterialIcons',max_length=100)
     fontPackage=models.CharField(null=True, max_length=100)
 
