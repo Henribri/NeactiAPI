aa41f78d80ad1c6dba1a52be9b43fb7493abeae1
Henribri <henri.briaux@viacesi.fr>
2020-08-28 21:56:13 +0200

    comment

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     path('events/', event_view.EventList.as_view()),
     path('events/<slug:pk>/', event_view.EventDetail.as_view()),
 
+    # Events by user
     path('events/user_registered/<slug:userId>/', event_view.UserEventList.as_view()),
     path('events/user_not_registered/<slug:userId>/', event_view.UserNoEventList.as_view()),
 
 
-
+    # Categories
     path('category/', category_view.CategoryList.as_view()),
     path('category/<slug:pk>/', category_view.CategoryDetail.as_view()),
 
